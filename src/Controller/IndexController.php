766b8652bc10c09072ba062055a91fc13f49a525
Eimantas Markevičius <klakalasas@gmail.com>
2018-04-21 21:57:21 +0300

    #SP-28: Added access control to sonata admin routes and redirecting already logged in users from login and registration routes to homepage.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
