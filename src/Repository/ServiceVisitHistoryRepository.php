d85a4dde5deaf9927a44cba4d90c7f4648295f88
NeringaLaimutyte <neringa.laimutyte@ktu.edu>
2018-05-10 07:12:40 -0700

    SP-27 Added repositories

---
+++ src/Repository/ServiceVisitHistoryRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ServiceVisitHistory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method ServiceVisitHistory|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ServiceVisitHistory|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ServiceVisitHistory[]    findAll()
+ * @method ServiceVisitHistory[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CountryRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, ServiceVisitHistory::class);
+    }
+}
---
+++ src/Repository/VisitStatusesRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\VisitStatuses;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method VisitStatuses|null find($id, $lockMode = null, $lockVersion = null)
+ * @method VisitStatuses|null findOneBy(array $criteria, array $orderBy = null)
+ * @method VisitStatuses[]    findAll()
+ * @method VisitStatuses[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CountryRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, VisitStatuses::class);
+    }
+}
