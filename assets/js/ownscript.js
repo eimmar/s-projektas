46f19115fe18c53b1af8ccb9a551c2ee6c4587a9
Eimantas Markevičius <klakalasas@gmail.com>
2018-05-20 13:57:00 +0300

    #SP-13: Added ajax update event whenever visit data is changed.

--- assets/js/ownscript.js
+++ assets/js/ownscript.js
@@ -1,6 +1,46 @@
 $(document).ready(function () {
     $('.address-type').collection();
 
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+    var visitForm = $('#visit-arrangement');
+    visitForm.on('change', 'select', function() {
+
+        var data = visitForm.serializeObject();
+        visitForm.addClass('form-loading');
+
+        $.ajax({
+            url: visitForm.attr('action'),
+            type: visitForm.attr('method'),
+            dataType: 'json',
+            data: data,
+            success: function(response) {
+                if (response.success) {
+                    var newForm = $('.form-content-container', $(response.html));
+                    visitForm.html(newForm);
+                }
+                visitForm.removeClass('form-loading');
+            }
+        });
+    });
+
+
+    $('.visit-service-type').collection();
+
     //TODO: Vehicle form based on manufacturer
     // $(function () {
     //     var manufacturer = $('#vehicle_manufacturer');
--- src/Form/VisitServiceType.php
+++ src/Form/VisitServiceType.php
@@ -11,21 +11,29 @@ namespace App\Form;
 
 use App\Entity\VisitService;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class VisitServiceType extends AbstractType
 {
+    const MAX_QTY = 10;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $qtyOptions = [];
+        for ($i = 1; $i <= self::MAX_QTY; $i++) {
+            $qtyOptions[$i] = $i;
+        }
+
         $builder
             ->add('name', HiddenType::class)
             ->add('description', HiddenType::class)
             ->add('duration', HiddenType::class)
             ->add('price', HiddenType::class)
-            ->add('quantity', HiddenType::class)
-            ->add('visit', HiddenType::class, ['property_path' => 'id'])
+            ->add('quantity', ChoiceType::class, ['choices' => $qtyOptions])
+            ->add('service', HiddenType::class, ['property_path' => 'id'])
         ;
     }
 
